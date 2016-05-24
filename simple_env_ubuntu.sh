2278bdc9132198071215936f6b12ee1267e6df71
Natan Facchin <natan.facchin@hp.com>
2016-05-24 16:12:50 -0300

    Added basic Ubuntu 16.06 script for Java/Python

---
+++ simple_env_ubuntu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+cd ~/Downloads;
+sudo add-apt-repository ppa:webupd8team/java;
+sudo apt-get update;
+sudo apt-get install -y oracle-java8-installer;
+sudo apt-get install -y libqt4-opengl;
+sudo apt-get install -y dkms;
+sudo apt-get install -y virtualbox virtualbox-ext-pack;
+sudo apt-get install -y vagrant;
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -;
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list';
+sudo apt-get update;
+sudo apt-get install google-chrome-stable;
+sudo apt-get install -y maven;
+sudo gem install cucumber;
+sudo apt-get install -y git;
+sudo apt-get install -y python-software-properties;
+sudo apt-get install -y python python-pip;
+pip install --upgrade pip;
+sudo update-java-alternatives -s /usr/lib/jvm/java-8-oracle/;
