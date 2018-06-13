ccccdaacddd88e3d6b34b78e0848834b934ebbd3
Natan Facchin <maxmousee@gmail.com>
2018-06-13 20:28:11 +0200

    Added MacOS file

---
+++ simple_env_macos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# Install basic tools to simplify my life in MacOS
+
+# First, install npm, JDK, VSCode
+
+# Then install nativefier
+
+sudo npm install nativefier -g;
+
+cd ~/Desktop;
+
+nativefier "http://www.messenger.com";
+
+nativefier "m.instagram.com" -u "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.25 (KHTML, like Gecko) Version/11.0 Mobile/15A5304j Safari/604.1";
+
