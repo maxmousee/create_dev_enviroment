fcaa90de635cd709e99876b9a2730721d74b05e3
Natan Facchin <maxmousee@gmail.com>
2019-02-25 19:40:06 +0100

    Update instagram max-width

--- simple_env_macos.sh
+++ simple_env_macos.sh
@@ -37,6 +37,6 @@ cd ~/Desktop;
 
 nativefier "http://www.messenger.com";
 
-nativefier "m.instagram.com" -u "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1";
+nativefier "m.instagram.com" --max-width "600" -u "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1";
 
 nativefier --name "Microsoft To-Do" --internal-urls ".*?\.microsoft\.*?"  "https://to-do.microsoft.com/"
